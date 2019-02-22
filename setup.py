80c567b7fb57d09cffaaf8500f9d57865c9c920b
Eric Torres <erictorres4@protonmail.com>
2019-02-22 07:54:56 -0800

    Remove git version-checking code from setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 import setuptools
-import subprocess
 
 with open("README.rst", "r") as fh:
     long_description = fh.read()
 
-def get_version():
-    cmd = 'printf "%s" "$(git describe --long | sed "s/\([^-]*-\)g/r\1/;s/-/./g")"'
-    return subprocess.run(cmd, shell=True, text=True).stdout
-
 setuptools.setup(
     name="packaging_scripts",
-    version=get_version(),
+    version="1.0",
     author="Eric Russel Torres",
     author_email="erictorres4@protonmail.com",
     description="A set of helpers for automating borg interaction",
