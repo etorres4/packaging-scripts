21791e63c6ae669db86553f143758ab432f3f6a0
Eric Torres <erictorres4@protonmail.com>
2019-03-27 17:20:59 -0700

    Use setuptools.find_packages to locate packages

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/plain",
     url="",
-    packages=["packaging_scripts"],
+    packages=setuptools.find_packages(),
     scripts=["bin/addpkg", "bin/delpkg", "bin/fqo"],
     classifiers=[
         "Programming Language :: Python :: 3",
