edfc99681913724f2691aaa53117c876618941fb
Eric Torres <erictorres4@protonmail.com>
2024-05-09 20:14:43 -0700

    Keep setup.py minimal

--- setup.py
+++ setup.py
@@ -1,15 +1,5 @@
 import setuptools
 
-# ========== Constants ==========
 EXCLUDED_PACKAGES = ["test", "tests"]
 PACKAGES = setuptools.find_packages(exclude=EXCLUDED_PACKAGES)
-SCRIPTS = ["bin/addpkg", "bin/delpkg", "bin/fqo", "bin/pug2"]
-
-
-# ========== Functions ==========
-with open("README.rst", "r") as fh:
-    long_description = fh.read()
-
-
-# ========== Package Setup ==========
 setuptools.setup(packages=PACKAGES)
