f51b76ec6d09abae61131e77f263f1e922be72c3
Eric Torres <erictorres4@protonmail.com>
2024-05-12 01:10:25 -0700

    Re-add scripts directive to setup.py

--- setup.py
+++ setup.py
@@ -2,4 +2,10 @@ import setuptools
 
 EXCLUDED_PACKAGES = ["test", "tests"]
 PACKAGES = setuptools.find_packages(exclude=EXCLUDED_PACKAGES)
-setuptools.setup(packages=PACKAGES)
+SCRIPTS = [
+    "packaging_scripts/bin/addpkg",
+    "packaging_scripts/bin/delpkg",
+    "packaging_scripts/bin/fqo",
+    "packaging_scripts/bin/pug2",
+]
+setuptools.setup(scripts=SCRIPTS, packages=PACKAGES)
